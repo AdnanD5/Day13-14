828d8c774204f29078c58bb3fa47d0c02d454cbf
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-21 19:24:18 +0100

    nepotpuno

---
+++ ex01/scripts/index.js
@@ -0,0 +1,9 @@
+var modal = document.getElementsByClassName("modal");
+
+var modalBtn = document.getElementById("");
+
+modal.addEventListener("click", openModal);
+
+function openModal(){
+modal.style.display = "block";
+}
